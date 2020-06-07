057802af2d959dfbbe8cdcffe653f7ab1caf214b
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-07 11:32:25 -0300

    Serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,10 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-  res.send('Hello express!')
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About page</h1>')
-})
+app.use(express.static(publicDirPath))
 
 app.get('/weather', (req, res) => {
   res.send({

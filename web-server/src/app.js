efcfd115acec6fc85d4dd4de0d4cdc00034f78b1
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-07 12:25:04 -0300

    Advanced templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
-const viewsDirPath = path.join(__dirname, '../templates')
+const viewsDirPath = path.join(__dirname, '../templates/views')
+const partialsDIrPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
 app.set('views', viewsDirPath)
+hbs.registerPartials(partialsDIrPath)
 
 app.use(express.static(publicDirPath))
 
@@ -26,7 +29,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-      helpText: 'This is some helpful text.'
+      helpText: 'This is some helpful text.',
+      title: 'Help',
+      name: 'Jorge Souza'
   })
 })
 

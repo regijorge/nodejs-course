5955a801f7809a7ea85f2ffa26e3a8afcc278d9a
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-07 12:00:33 -0300

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    name: 'Jorge Souza'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+      title: 'About Me',
+      name: 'Jorge Souza'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+      helpText: 'This is some helpful text.'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({

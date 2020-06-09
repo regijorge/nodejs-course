2ee790b2765e4b86a0fce294b3a97d0e0e7c5939
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-09 20:33:41 -0300

    Deploying to Heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDirPath = path.join(__dirname, '../public')
 const viewsDirPath = path.join(__dirname, '../templates/views')
 const partialsDIrPath = path.join(__dirname, '../templates/partials')
@@ -73,6 +75,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up and running on http://localhost:3000')
 })

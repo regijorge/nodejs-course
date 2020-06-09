61595b4439edbb9da4b0775d043f0e203550dc04
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-09 09:44:25 -0300

    Building a JSON HTTP endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
@@ -36,16 +38,30 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-  if (!req.query.address) {
+  const address = req.query.address
+
+  if (!address) {
     return res.send({
       error: 'You must provide an address!'
     })
   }
 
-  res.send({
-    forecast: '17',
-    location: 'Brazil',
-    address: req.query.address
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({error})
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({error})
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address
+      })
+    })
   })
 })
 

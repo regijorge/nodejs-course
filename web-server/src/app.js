78e51042511fbaca2acea2f4e936f98f88aa2a21
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-09 09:55:30 -0300

    ES6 aside: Default functions parameters

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,7 +46,7 @@ app.get('/weather', (req, res) => {
     })
   }
 
-  geocode(address, (error, { latitude, longitude, location }) => {
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
     if (error) {
       return res.send({error})
     }

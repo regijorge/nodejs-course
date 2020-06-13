d4d4d9dca0ffbe11f3651c400dcec4aa8058c8b2
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-13 10:24:19 -0300

    Inserting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -18,5 +18,46 @@ MongodbClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) 
   db.collection('users').insertOne({
     name: 'Jorge Souza',
     age: 27
+  }, (error, result) => {
+    if (error) {
+      return console.log(`Unable to insert User: ${error}`)
+    }
+
+    console.log(result.ops)
+  })
+
+  db.collection('users').insertMany([
+    {
+      name: 'Carlucho',
+      age: 35
+    }, {
+      name: 'Luíz Pinguço',
+      age: 72
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unabel to insert documents!')
+    }
+
+    console.log(result.ops)
+  })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Clean the house',
+      completed: false
+    }, {
+      description: 'Study nodejs course',
+      completed: false
+    }, {
+      description: 'Buy food',
+      completed: true
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unable to insert tasks')
+    }
+
+    console.log(result.ops)
   })
 })

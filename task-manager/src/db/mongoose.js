c57a2f7eb24277b71ff7064799c30cefc5c50e96
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-13 13:03:41 -0300

    Create a mongoose Model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -7,3 +7,43 @@ mongoose.connect(connectionURL, {
   useNewUrlParser: true,
   useCreateIndex: true
 })
+
+const User = mongoose.model('User', {
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  }
+})
+
+const me = new User({
+  name: 'Jorge Souza',
+  age: 27
+})
+
+// me.save().then(() => {
+//   console.log(me)
+// }).catch(error => {
+//   console.log(error)
+// })
+
+const Task = mongoose.model('Task', {
+  descripton: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
+})
+
+const myTask = new Task({
+  descripton: 'Clean the house',
+  completed: false
+})
+
+// myTask.save().then(() => {
+//   console.log(myTask)
+// }).catch(error => {
+//   console.log(error)
+// })

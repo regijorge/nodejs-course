16b5041f2abaf493089b40310f9a1a62439c84ae
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-18 20:08:25 -0300

    Enviroment variables

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,4 @@
 const { MongoClient, ObjectID } = require('mongodb')
-require('dotenv').config()
 
 const connectionURL = process.env.MONGODB_URI
 
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
 
 const connectionURL = process.env.MONGODB_URI
 

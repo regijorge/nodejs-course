ce68401a71d87631ccf19896d5934f03d65faaf2
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-17 21:18:58 -0300

    Sorting data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -19,9 +19,15 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
   const match = {}
+  const sort = {}
 
   if (req.query.completed) {
     match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sort) {
+    const parts = req.query.sort.split(':')
+    sort[parts[0]] = parts[1] === 'asc' ? 1 : -1
   }
 
   try {
@@ -30,7 +36,8 @@ router.get('/tasks', auth, async (req, res) => {
       match,
       options: {
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort
       }
     }).execPopulate()
     res.send(req.user.tasks)

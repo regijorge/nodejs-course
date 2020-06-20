585827e576e71cb113eb35c878b918f362384ca3
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-20 12:10:58 -0300

    Getting started with socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,13 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = new express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
 const publicDir = path.join(__dirname, '../public')
 
 app.use(express.static(publicDir))
@@ -10,6 +16,10 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000, () => {
+io.on('connection', () => {
+  console.log('Socket io is connected')
+})
+
+server.listen(port, () => {
   console.log('Server is running')
 })

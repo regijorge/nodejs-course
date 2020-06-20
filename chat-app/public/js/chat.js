fe0a51bffc7e95210d55c8f37c5a1cb8c47fa1f3
Jorge Souza <regi.jorge.s@gmail.com>
2020-06-20 18:05:59 -0300

    Rendering user list

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -20,6 +20,14 @@ socket.on('locationMessage', (message) => {
   $messages.insertAdjacentHTML('beforeend', html)
 })
 
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users
+  })
+  document.querySelector('#sidebar').innerHTML = html
+})
+
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
@@ -28,6 +36,7 @@ const $messages = document.querySelector('#messages')
 
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 const {
   username,
